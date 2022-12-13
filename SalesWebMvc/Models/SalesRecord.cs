7d82baaed6c45b9fdbe0c66edfa2e7a9bbdaa1bb
Rogerio <rogerio1597@gmail.com>
2022-12-12 21:17:37 -0300

    Implementando busca agrupada.

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -1,4 +1,6 @@
-﻿public class SalesRecord
+﻿using System.ComponentModel.DataAnnotations;
+
+public class SalesRecord
 {
     public SalesRecord() { }
 
@@ -10,9 +12,12 @@
         Status = status;
         Seller = seller;
     }
-
     public int Id { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
     public DateTime Date { get; set; }
+
+    [DisplayFormat(DataFormatString = "{0:F2}")]
     public double Amount { get; set; }
     public SaleStatus Status { get; set; }
     public Seller Seller { get; set; }
--- SalesWebMvc/Services/SalesRecordService.cs
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -1,20 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
+using SalesWebMvc.Models.ViewModels;
+
 public class SalesRecordService
 {
     private readonly SalesWebMvcContext _context;
-
     public SalesRecordService(SalesWebMvcContext context)
     {
         _context = context;
     }
-
     public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
     {
         var result = from obj in _context.SalesRecord select obj; // retorna um objeto IQueryable do tipo DbSet(No caso DbSet<SalesRecord).
         if (minDate.HasValue)
         {
-            result = result.Where(x => x.Date >= minDate.Value); 
+            result = result.Where(x => x.Date >= minDate.Value);
         }
         if (maxDate.HasValue)
         {
@@ -27,5 +27,26 @@ public class SalesRecordService
             .OrderByDescending(x => x.Date)
             .ToListAsync();
     }
+    public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+    {
+        var result = from obj in _context.SalesRecord select obj;
+        if (minDate.HasValue)
+        {
+            result = result.Where(x => x.Date >= minDate.Value);
+        }
+        if (maxDate.HasValue)
+        {
+            result = result.Where(x => x.Date <= maxDate.Value);
+        }
 
+        // Solução para consertar o erro desse endpoint devido a estarmos no .NET 6
+
+        var data = await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+
+        return data.GroupBy(x => x.Seller.Department).ToList();
+    }
 }
